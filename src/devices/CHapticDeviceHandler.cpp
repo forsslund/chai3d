c44415ec908cf1a1b391dafbc617ee802c09529d
forsslund <jofo02@kth.se>
2017-03-16 18:43:21 +0100

    added woodenhaptics files

--- src/devices/CHapticDeviceHandler.cpp
+++ src/devices/CHapticDeviceHandler.cpp
@@ -68,6 +68,10 @@
 #include "devices/CSixenseDevices.h"
 #endif
 
+#if defined(C_ENABLE_WOODEN_DEVICE_SUPPORT)
+#include "devices/CWoodenDevice.h"
+#endif
+
 #if defined(C_ENABLE_CUSTOM_DEVICE_SUPPORT)
 #include "devices/CMyCustomDevice.h"
 #endif
@@ -211,6 +215,26 @@ void cHapticDeviceHandler::update()
     for (int i=0; i<count; i++)
     {
         device = cPhantomDevice::create(i);
+        m_devices[m_numDevices] = device;
+        m_numDevices++;
+    }
+
+    #endif
+
+
+    //--------------------------------------------------------------------------
+    // search for Wooden Haptics (Sensoray 826 or USB) device
+    //--------------------------------------------------------------------------
+    #if defined(C_ENABLE_WOODEN_DEVICE_SUPPORT)
+
+    // check for how many devices are available for this class of devices
+    // (Note however that WoodenDevice will currently always return 1 device.
+    count = cWoodenDevice::getNumDevices();
+
+    //  open all remaining devices
+    for (int i=0; i<count; i++)
+    {
+        device = cWoodenDevice::create(i);
         m_devices[m_numDevices] = device;
         m_numDevices++;
     }
